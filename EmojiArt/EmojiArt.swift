0d098f8ec6ca27a66d82b43a9a20e1fa2e25cdcb
Barry Martin <bymartin@users.noreply.github.com>
2020-06-13 22:02:26 -0500

    Lecture 7: Mulithreading EmojiArt

---
+++ EmojiArt/EmojiArt.swift
@@ -0,0 +1,44 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//  Model
+//
+//  Created by Barry Martin on 6/12/20.
+//  Copyright © 2020 Barry Martin. All rights reserved.
+//
+
+import Foundation
+
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        
+        // have 0,0 be in center of screen
+        // unlike iOS
+        var x: Int // offset from center
+        var y: Int // offset from center
+        var size: Int
+        //var id = UUID() // overkill for this
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+        
+    }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+    
+}
