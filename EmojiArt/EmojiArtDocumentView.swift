d7a97e5cfb87d5bc50f2e82faea23a12ad026926
Barry Martin <bymartin@users.noreply.github.com>
2020-06-17 15:37:48 -0500

    Assignment 4: in progress - Can now tap to select one or more emojis - Tap again to deselect an emoji - Single tap on background to deselect all emojis - Used .gesture.exclusively(before:) to ensure double-tap on background takes precedence over single tap - used Set to keep track of selected emojis with extension toggleMatching function

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -29,33 +29,60 @@ struct EmojiArtDocumentView: View {
                     Color.white.overlay(
                         OptionalImage(uiImage: self.document.backgroundImage)
                             .scaleEffect(self.zoomScale)
-                        .offset(self.panOffset)
+                            .offset(self.panOffset)
                     )
-                        .gesture(self.doubleTapToZoom(in: geometry.size))
+                        .gesture(
+                            self.doubleTapToZoom(in: geometry.size)
+                                .exclusively(before: self.singleTapToSelect(for: nil))
+                    )
                     ForEach(self.document.emojis) { emoji in
                         Text(emoji.text)
                             //.font(self.font(for: emoji))
                             .font(animatableWithSize: emoji.fontSize * self.zoomScale)
                             .position(self.position(for: emoji, in: geometry.size))
+                            .gesture(self.singleTapToSelect(for: emoji))
+                            .shadow(color: self.isEmojiSelected(emoji) ? .black : .clear , radius: 10 )
                         
                     }
                 }
-            .clipped()
-                    .gesture(self.panGesture())
-                    .gesture(self.zoomGesture())
-            .edgesIgnoringSafeArea([.horizontal, .bottom])
-            .onDrop(of: ["public.image","public.text"], isTargeted: nil) { providers, location in
-                // SwiftUI bug (as of 13.4)? the location is supposed to be in our coordinate system
-                // however, the y coordinate appears to be in the global coordinate system
-                //var location = geometry.convert(location, from: .global)
-                var location = CGPoint(x: location.x, y: geometry.convert(location, from: .global).y)
-                location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
-                location = CGPoint(x: location.x - self.panOffset.width, y: location.y - self.panOffset.height)
-                location = CGPoint(x: location.x / self.zoomScale, y: location.y / self.zoomScale)
-                return self.drop(providers: providers, at: location)
+                .clipped()
+                .gesture(self.panGesture())
+                .gesture(self.zoomGesture())
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+                .onDrop(of: ["public.image","public.text"], isTargeted: nil) { providers, location in
+                    // SwiftUI bug (as of 13.4)? the location is supposed to be in our coordinate system
+                    // however, the y coordinate appears to be in the global coordinate system
+                    //var location = geometry.convert(location, from: .global)
+                    var location = CGPoint(x: location.x, y: geometry.convert(location, from: .global).y)
+                    location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                    location = CGPoint(x: location.x - self.panOffset.width, y: location.y - self.panOffset.height)
+                    location = CGPoint(x: location.x / self.zoomScale, y: location.y / self.zoomScale)
+                    return self.drop(providers: providers, at: location)
                 }
             }
         }
+    }
+    
+    @State private var selectedEmojis = Set<EmojiArt.Emoji>()
+    
+    private func singleTapToSelect(for emoji: EmojiArt.Emoji?) -> some Gesture {
+        TapGesture(count: 1)
+            .onEnded {
+                if let emoji = emoji {
+                    print("Tapped emoji id: \(emoji.id)")
+                    self.selectedEmojis.toggleMatching(emoji)
+                } else {
+                    // single tap on background
+                    // deselect all emojis
+                    self.selectedEmojis.removeAll()
+                }
+                
+                
+        }
+    }
+    
+    private func isEmojiSelected(_ emoji: EmojiArt.Emoji) -> Bool {
+        selectedEmojis.contains(matching: emoji)
     }
     
     @State private var steadyStateZoomScale: CGFloat = 1.0
@@ -74,13 +101,22 @@ struct EmojiArtDocumentView: View {
         }
     }
     
+    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            self.steadyStatePanOffset = .zero
+            self.steadyStateZoomScale = min(hZoom, vZoom)
+        }
+    }
+    
     private func zoomGesture() -> some Gesture {
         MagnificationGesture()
             .updating($gestureZoomScale) { latestGestureScale, gestureZoomScale, transaction in
                 gestureZoomScale = latestGestureScale
         }
-            .onEnded { finalGestureScale in
-                self.steadyStateZoomScale *= finalGestureScale
+        .onEnded { finalGestureScale in
+            self.steadyStateZoomScale *= finalGestureScale
         }
     }
     
@@ -101,18 +137,11 @@ struct EmojiArtDocumentView: View {
         }
     }
     
-    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
-        if let image = image, image.size.width > 0, image.size.height > 0 {
-            let hZoom = size.width / image.size.width
-            let vZoom = size.height / image.size.height
-            self.steadyStatePanOffset = .zero
-            self.steadyStateZoomScale = min(hZoom, vZoom)
-        }
-    }
     
-//    private func font(for emoji: EmojiArt.Emoji) -> Font {
-//        Font.system(size: emoji.fontSize * zoomScale)
-//    }
+    
+    //    private func font(for emoji: EmojiArt.Emoji) -> Font {
+    //        Font.system(size: emoji.fontSize * zoomScale)
+    //    }
     
     private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
         var location = emoji.location
